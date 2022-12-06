f055d9faf2554154299f24575e564df939de4900
Davide Mauri <damauri@microsoft.com>
2022-12-06 11:45:01 -0800

    added comments and table variable use

--- 10-things/03-transactions-and-errors.sql
+++ 10-things/03-transactions-and-errors.sql
@@ -1,17 +1,20 @@
 -- DB: AzureDataConference
 
+-- Errors in ad-hoc queries / scripts
+
 drop table if exists dbo.timesheet_test
 select * into dbo.timesheet_test from dbo.timesheet
 go
 
 select * from dbo.timesheet_test where id = 6
 
+-- excute this script all together
 declare @n int = 0;
 begin tran
 
 	delete from dbo.timesheet_test where id = 6
 
-	select 1 / @n;
+	select 1 / @n; -- Generate an error
 
 commit tran
 
@@ -19,7 +22,7 @@ select @@trancount
 select * from dbo.timesheet_test where id = 6
 go
 
------
+-- Test now the same script using a Stored Procedure
 
 drop table if exists dbo.timesheet_test
 select * into dbo.timesheet_test from dbo.timesheet
@@ -46,7 +49,7 @@ select @@trancount
 select * from dbo.timesheet_test where id = 6
 go
 
------
+-- Update the stored procedure
 
 drop table if exists dbo.timesheet_test
 select * into dbo.timesheet_test from dbo.timesheet
@@ -55,7 +58,7 @@ go
 create or alter procedure dbo.stp_test_error
 as
 begin
-	set xact_abort on;
+	set xact_abort on; -- automatically rollback and terminate
 	declare @n int = 0;
 	begin tran
 
@@ -75,7 +78,7 @@ select * from dbo.timesheet_test where id = 6
 go
 
 
------
+-- Manual error management
 
 drop table if exists dbo.timesheet_test
 select * into dbo.timesheet_test from dbo.timesheet
@@ -83,7 +86,10 @@ go
 
 create or alter procedure dbo.stp_test_error
 as
-begin	
+begin
+	-- This is a *variable* and therefore does not take part in a trasaction
+	declare @log table (id int not null identity primary key, [message] nvarchar(1000));
+
 	declare @n int = 0;
 	begin try
 		begin tran
@@ -94,8 +100,11 @@ begin
 		commit tran
 	end try
 	begin catch
+		insert into @log ([message]) values ('Error found, rolling back.');
+	
 		rollback
 	end catch
+	select * from @log
 end
 go
 
